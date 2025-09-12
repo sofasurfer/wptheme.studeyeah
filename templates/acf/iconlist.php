65ef6eee243f13590ed811c8ce7e90791e021395
sofasurfer <kilian@sofasurfer.ch>
2025-09-12 16:49:51 +0200

    Fix logo color

--- templates/acf/iconlist.php
+++ templates/acf/iconlist.php
@@ -1,7 +1,7 @@
 <div class="container">
     <div class="row stream-list">
         <?php foreach( $site_element['list'] as $icon ): ?>
-            <div class="col-md-4 col-xs-6"><a target="<?= $icon['link']['target']; ?>" href="<?= $icon['link']['url']; ?>"><?= wp_get_attachment_image($icon['icon'], 'full', false, ['class' => 'img-fluid']);?></a></div>
+            <div class="col-md-4 col-xs-6"><a target="<?= $icon['link']['target']; ?>" href="<?= $icon['link']['url']; ?>"><?= wp_get_attachment_image($icon['icon'], 'full', false, ['class' => 'img-fluid ']);?></a></div>
         <?php endforeach; ?>
     </div>
 </div>
